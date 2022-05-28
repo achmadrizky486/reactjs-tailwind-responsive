0f896954d6a1253de9bd2af37b09a4c4462a6a17
achmadrizky486 <achmadrizky486@gmail.com>
2022-05-28 20:17:39 +0700

    feat : add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold">Capstone 11</p>
+        <h1 className="md:text-7xl md:py-6 sm:text-6xl text-4xl font-bold">
+          Build with
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Dikasih info maseh
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(true);
 
   const handleNav = () => {
     setNav(!nav);
@@ -11,27 +11,27 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a] hover:text-sky-400">
-        hai
+        Hai
       </h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex ">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav}>
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            ? "fixed left-0 top-0 w-[60%] h-full bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%] top-0 w-[60%] h-full ease-in-out duration-500"
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] hover:text-sky-400 m-4">
-          HHHai
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] hover:text-sky-400 mt-8 m-4">
+          Hai
         </h1>
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
