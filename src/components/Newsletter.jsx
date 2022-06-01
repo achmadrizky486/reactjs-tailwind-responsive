bfddb90cf236f3b51d507e5a227fbeb5229f2577
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-01 18:03:51 +0700

    feat : add footer

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,10 +5,12 @@ const Newsletter = () => {
     <div className="w-full py-16 text-white px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
         <div className="lg:col-span-2 my-5">
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 mx-10">
             Want tips and tricks to optimize your flow?
           </h1>
-          <p>Sign up to our newsletter and stay up to date</p>
+          <p className="md:mx-10 sm:mx-0 mx-10">
+            Sign up to our newsletter and stay up to date
+          </p>
         </div>
         <div className="m-4">
           <div className="flex flex-col sm:flex-row items-center justify-between w-full">
